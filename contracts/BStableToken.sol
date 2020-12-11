625b62249f8d9c959b1ef40ee709f63608a767a4
张乐平 <holynull@126.com>
2020-12-11 10:34:44 +0800

    init supply and init rate

--- contracts/BStableToken.sol
+++ contracts/BStableToken.sol
@@ -10,10 +10,10 @@ contract BStableToken is BEP20("bStable DAO Token", "BST"), Ownable {
     address minter;
 
     uint256 YEAR = uint256(86400).mul(365);
-    uint256 INITIAL_SUPPLY = uint256(1_303_030_303).mul(10**18);
-    uint256 INITIAL_RATE = uint256(274_815_283).mul(10**18).div(YEAR);
+    uint256 INITIAL_SUPPLY = uint256(40_000_000_000).mul(10**18); // distribute to employers, investors, and consultants.
+    uint256 INITIAL_RATE = uint256(1_909_243_017).mul(10**18).div(YEAR); // first year release amounts for farming
     uint256 RATE_REDUCTION_TIME = YEAR;
-    uint256 RATE_REDUCTION_COEFFICIENT = 1189207115002721024;
+    uint256 RATE_REDUCTION_COEFFICIENT = 1189207115002721024; // 1/4 power of 2
     uint256 INFLATION_DELAY = 86400;
 
     int128 mining_epoch;
